dc949652483033f50874cdd6d24a46390a490c7c
“Luis <l.garcia24@hotmail.com>
2020-12-18 13:30:02 -0600

    add fetch request for login

--- Components/App2.js
+++ Components/App2.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
 import React, { Component, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AsyncStorage } from '@react-native-community/async-storage';
 import Login from './Components/Login'
 import Feed from './Components/Feed'
 import Details from './Components/Details'
@@ -17,6 +18,24 @@ export default function App2({ navigation }) {
     const [user, setUser] = useState({})
     const [alert, setAlert] = useState({})
 
-
+    const login = (username, password) => {
+        fetch('http://localhost3000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.error) {
+                    setAlerts(data.errors)
+                } else {
+                    AsyncStorage.setItem('token', data.token)
+                    setUser(data.user)
+                    setAlerts([])
+                }
+            })
+    }
 
 }
